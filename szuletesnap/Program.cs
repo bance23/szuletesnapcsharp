c22223176dbae3816600761fef8ee6a632b3f032
bence <bence@DESKTOP-AA5IA6A>
2022-09-07 23:03:33 +0200

    Add project files.

---
+++ szuletesnap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szuletesnap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia! Hány emberen vizsgáljuk az esélyeket?");
+            int emberszam = Convert.ToInt32(Console.ReadLine());
+
+            double tarolo = 0;
+
+            for (int i = 0; i < emberszam; i++)
+            {
+                tarolo = tarolo + i;
+                Console.WriteLine(tarolo);
+            }
+
+            double esely = tarolo / 3.65;
+            Console.WriteLine(esely);
+            Console.WriteLine("{0} embernél {1} % esély van rá, hogy van két olyan ember akik ugyanazon a napon ünneplik a születésnapjukat.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ szulinapgenerator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szulinapgenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia! Hány emberen vizsgáljuk az szimulációt?");
+            int emberszam = Convert.ToInt32(Console.ReadLine()); Random random = new Random();
+
+            int[] szulinapok = new int[emberszam];
+
+            for (int i = 0; i < szulinapok.Length; i++)
+            {
+                szulinapok[i] = random.Next(1,366);
+                Console.WriteLine(szulinapok[i]);
+            }
+
+            for (int i = 0; i < szulinapok.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < szulinapok.Length; j++)
+                {
+
+                    if (szulinapok[i] == szulinapok[j])
+                        count = count + 1;
+                }
+                if (count > 1)
+                Console.WriteLine(count + "embernek ugyanakkor ünnepli a születésnapját.");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
